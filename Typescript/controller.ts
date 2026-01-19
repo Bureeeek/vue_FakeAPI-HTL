487e8fec6a63f5eae7680cbb46a16dbcff9fa52d
Bureeeek <burakkocbk653@gmail.com>
2026-01-19 08:05:05 +0100

    rest
    test

--- Typescript/controller.ts
+++ Typescript/controller.ts
@@ -1,4 +1,4 @@
-import { Autogeschaeft1 } from "./autogeschaeft1.js";
+wimport { Autogeschaeft1 } from "./autogeschaeft1.js";
 import { Autogeschaeft2 } from "./autogeschaeft2.js";
 import { CarController } from "./carController.js";
 
